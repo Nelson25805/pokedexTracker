2fcd7093fae5a7de0e24cffed9137442b32b42c9
Nelson <Nelson25805@hotmail.com>
2025-01-30 12:06:42 -0500

    removed unnecessary code from file

--- PokedexTracker/Models/PokemonGeneration.cs
+++ PokedexTracker/Models/PokemonGeneration.cs
@@ -3,14 +3,12 @@
     public string Name { get; set; }
     public string[] Speeches { get; set; } // Initial dialogues before name submission
     public string[] ProfessorImages { get; set; }
-    public string[] PostNameSubmissionSpeeches { get; set; } // Post-name submission dialogues
 
     // Constructor that accepts four parameters
-    public PokemonGeneration(string name, string[] speeches, string[] professorImages, string[] postNameSubmissionSpeeches)
+    public PokemonGeneration(string name, string[] speeches, string[] professorImages)
     {
         Name = name;
         Speeches = speeches;
         ProfessorImages = professorImages;
-        PostNameSubmissionSpeeches = postNameSubmissionSpeeches;
     }
 }
