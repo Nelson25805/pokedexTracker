be0fd83037f18f6a19d6ccacc3003d066a1afcd5
Nelson <Nelson25805@hotmail.com>
2025-05-05 19:48:49 -0400

    added subfolders for gen version for different diplomas

--- PokedexTracker/Managers/AssetManager.cs
+++ PokedexTracker/Managers/AssetManager.cs
@@ -1,5 +1,6 @@
 ﻿// AssetManager.cs
 using System;
+using System.Collections.Generic;
 using System.IO;
 
 namespace PokedexTracker
@@ -61,30 +62,64 @@ namespace PokedexTracker
 
         public string GetDiplomaPath(string gameName, string mediaVersion = "GB", string printVersion = "Regular")
         {
-            // Base diplomas folder
+            // Map each game to its generation folder:
+            var genMap = new Dictionary<string, string>(StringComparer.OrdinalIgnoreCase)
+            {
+                // Gen I
+                ["Red"] = "Gen1",
+                ["Blue"] = "Gen1",
+                ["Yellow"] = "Gen1",
+                // Gen II
+                ["Gold"] = "Gen2",
+                ["Silver"] = "Gen2",
+                ["Crystal"] = "Gen2",
+                // Gen III
+                ["Ruby"] = "Gen3",
+                ["Sapphire"] = "Gen3",
+                ["Emerald"] = "Gen3",
+                ["Fire Red"] = "Gen3",
+                ["Leaf Green"] = "Gen3",
+                // Gen IV
+                ["Diamond"] = "Gen4",
+                ["Pearl"] = "Gen4",
+                ["Platinum"] = "Gen4",
+                ["Heart Gold"] = "Gen4",
+                ["Soul Silver"] = "Gen4",
+                // … add Gen V+ as needed …
+            };
+
+            // Determine gen folder, default to top-level if unknown:
+            string genFolder = genMap.TryGetValue(gameName, out var g) ? g : "";
+
+            // Determine game folder (e.g. “Red-Blue”, “Gold-Silver”, etc.)
+            string gameFolder;
+            if (gameName == "Red" || gameName == "Blue")
+                gameFolder = "Red-Blue";
+            else if (gameName == "Gold" || gameName == "Silver")
+                gameFolder = "Gold-Silver";
+            else if (gameName == "Ruby" || gameName == "Sapphire")
+                gameFolder = "Ruby-Sapphire";
+            else if (gameName == "Fire Red" || gameName == "Leaf Green")
+                gameFolder = "FireRed-LeafGreen";
+            else if (gameName == "Diamond" || gameName == "Pearl")
+                gameFolder = "Diamond-Pearl";
+            else if (gameName == "Heart Gold" || gameName == "Soul Silver")
+                gameFolder = "HeartGold-SoulSilver";
+            else
+                gameFolder = gameName;
+
+            // Build the diplomas root:
             string diplomasRoot = Path.Combine(_baseAssetsPath, "Diplomas");
 
-            // Determine folder name
-            string folderName;
-            if (gameName == "Red" || gameName == "Blue")
-                folderName = "Red-Blue";
-            else if (gameName == "Gold" || gameName == "Silver")
-                folderName = "Gold-Silver";
-            else if (gameName == "Ruby" || gameName == "Sapphire")
-                folderName = "Ruby-Sapphire";
-            else if (gameName == "Fire Red" || gameName == "Leaf Green")
-                folderName = "FireRed-LeafGreen";
-            else if (gameName == "Diamond" || gameName == "Pearl")
-                folderName = "Diamond-Pearl";
-            else if (gameName == "Heart Gold" || gameName == "Soul Silver")
-                folderName = "HeartGold-SoulSilver";
-            else
-                folderName = gameName;
+            // If we have a genFolder, look under it:
+            string targetDir = string.IsNullOrEmpty(genFolder)
+                ? diplomasRoot
+                : Path.Combine(diplomasRoot, genFolder);
 
-            // Compose file name with both versions: e.g., "Yellow-GB-Regular.png"
-            string fileName = $"{folderName}-{mediaVersion}-{printVersion}.png";
+            // Compose the filename: e.g. “Yellow-GB-Printer.png”
+            string fileName = $"{gameFolder}-{mediaVersion}-{printVersion}.png";
 
-            return Path.Combine(diplomasRoot, fileName);
+            return Path.Combine(targetDir, fileName);
         }
     }
 }
